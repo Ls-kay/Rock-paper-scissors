72a10433ebf856b61991df3c5254de1bcb8ace90
Lebohang M <msaselebohang@gmail.com>
2025-03-08 15:34:43 +0200

    removed comments

--- Main.py
+++ Main.py
@@ -1,38 +1,5 @@
 def play(player1, player2):
 
-    # if a == b:
-    #     return "Its a tie"
-
-    # if "rock" == a:
-    #     if b in ["paper", "spock"]:
-    #         return "B wins"
-    #     else:
-    #         return "A wins"
-
-    # if "paper" == a:
-    #     if b in ["scissors", "lizard"]:
-    #         return "B wins"
-    #     else:
-    #         return "A wins"
-
-    # if "scissors" == a:
-    #     if b in ["rock", "spock"]:
-    #         return "B wins"
-    #     else:
-    #         return "A wins"
-
-    # if "lizard" == a:
-    #     if b in ["scissors", "rock"]:
-    #         return "B wins"
-    #     else:
-    #         return "A wins"
-
-    # if "spock" == a:
-    #     if b in ["paper", "lizard"]:
-    #         return "B wins"
-    #     else:
-    #         return "A wins"
-
     elements = {
         "rock": ["paper", "spock"],
         "paper": ["scissors", "lizard"],
